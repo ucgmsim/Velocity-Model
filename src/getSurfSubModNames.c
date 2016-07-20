daadab7c2f5a8592211ff23223ca7ed87fb92bfd
thomsonem <Ethan@Ethans-Mac-Pro.canterbury.ac.nz>
2016-07-21 11:55:29 +1200

    Updated Version numbers

--- src/getSurfSubModNames.c
+++ src/getSurfSubModNames.c
@@ -191,20 +191,24 @@ global_model_parameters *getGlobalModelParameters(char *modelVersion)
     }
 
     // Model Version 0.1, 1D velocity sub Model
-    else if(strcmp(modelVersion,"0.11") == 0)
+    else if(strcmp(modelVersion,"Test_0.1") == 0)
     {
         // define the number of surfaces and sub models
-        GLOBAL_MODEL_PARAMETERS->nSurf = 2;
-        GLOBAL_MODEL_PARAMETERS->nVeloSubMod = 1;
+        GLOBAL_MODEL_PARAMETERS->nSurf = 3;
+        GLOBAL_MODEL_PARAMETERS->nVeloSubMod = 2;
         
         // insert surface surface keywords and filenames
         GLOBAL_MODEL_PARAMETERS->surf[0] = "posInfSurf";
         GLOBAL_MODEL_PARAMETERS->globalSurfFilenames[0] = "Data/Global_Surfaces/posInf.in";
         GLOBAL_MODEL_PARAMETERS->surf[1] = "negInfSurf";
-        GLOBAL_MODEL_PARAMETERS->globalSurfFilenames[1] = "Data/Global_Surfaces/negInf.in";
+        GLOBAL_MODEL_PARAMETERS->globalSurfFilenames[1] = "Data/DEM/DEM.in";
+        GLOBAL_MODEL_PARAMETERS->surf[2] = "negInfSurf";
+        GLOBAL_MODEL_PARAMETERS->globalSurfFilenames[2] = "Data/Global_Surfaces/negInf.in";
         
         // insert velocity submodel keywords and filenames (if necessary)
-        GLOBAL_MODEL_PARAMETERS->veloSubMod[0] = "v1DsubMod";
+        GLOBAL_MODEL_PARAMETERS->veloSubMod[0] = "NaNsubMod";
+
+        GLOBAL_MODEL_PARAMETERS->veloSubMod[1] = "v1DsubMod";
         GLOBAL_MODEL_PARAMETERS->veloMod1dFileName[0] = "Cant1D_v1.fd_modfile";
         
         

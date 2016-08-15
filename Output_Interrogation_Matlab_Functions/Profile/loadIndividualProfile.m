0feab4f2f7a3949af37f749680bb4d130a5393ad
thomsonem <Ethan@Ethans-Mac-Pro.canterbury.ac.nz>
2016-08-16 09:33:43 +1200

    Added North Island Tomo

--- Output_Interrogation_Matlab_Functions/Profile/loadIndividualProfile.m
+++ Output_Interrogation_Matlab_Functions/Profile/loadIndividualProfile.m
@@ -3,61 +3,62 @@ function profile = loadIndividualProfile(directoryName)
 % struct 
 
 
-fidLog = fopen(strcat('/',directoryName,'/Log/','VelocityModelInputs.txt'));
-
-temp = fgetl(fidLog);
-temp = fgetl(fidLog); % disregard first two lines
-
-% version number
-line = fgetl(fidLog);
-tabLocation = strfind(line,sprintf('\t'));
-profile.version = str2double(line(tabLocation(1):end));
-
-temp = fgetl(fidLog);
-
-% lat
-line = fgetl(fidLog);
-tabLocation = strfind(line,sprintf('\t'));
-profile.lat = str2double(line(tabLocation(1):end));
-
-% lon
-line = fgetl(fidLog);
-tabLocation = strfind(line,sprintf('\t'));
-profile.lon = str2double(line(tabLocation(1):end));
-
-% depth limits
-line = fgetl(fidLog);
-tabLocation = strfind(line,sprintf('\t'));
-profile.zmin = -str2double(line(tabLocation(1):end));
-line = fgetl(fidLog);
-tabLocation = strfind(line,sprintf('\t'));
-profile.zmax = -str2double(line(tabLocation(1):end));
-line = fgetl(fidLog);
-tabLocation = strfind(line,sprintf('\t'));
-profile.res = str2double(line(tabLocation(1):end));
-
-fclose(fidLog);
-
-% open surface depths file
-fidSurfDep = fopen(strcat('/',directoryName,'/','BasinSurfaceDepths.txt'));
-
-temp = fgetl(fidSurfDep);
-
-count = 1;
-while ~feof(fidSurfDep)
-    line = fgetl(fidLog);
-    tabLocation = strfind(line,sprintf('\t'));
-    profile.surfName{count} = line(1:tabLocation(1)-2);
-    profile.surfDep(count) = str2double(line(tabLocation(1):end));
-    count = count + 1;
-    
-end
-fclose(fidSurfDep);
+% fidLog = fopen(strcat('/',directoryName,'/Log/','VelocityModelInputs.txt'));
+% 
+% temp = fgetl(fidLog);
+% temp = fgetl(fidLog); % disregard first two lines
+% 
+% % version number
+% line = fgetl(fidLog);
+% tabLocation = strfind(line,sprintf('\t'));
+% profile.version = str2double(line(tabLocation(1):end));
+% 
+% temp = fgetl(fidLog);
+% 
+% % lat
+% line = fgetl(fidLog);
+% tabLocation = strfind(line,sprintf('\t'));
+% profile.lat = str2double(line(tabLocation(1):end));
+% 
+% % lon
+% line = fgetl(fidLog);
+% tabLocation = strfind(line,sprintf('\t'));
+% profile.lon = str2double(line(tabLocation(1):end));
+% 
+% % depth limits
+% line = fgetl(fidLog);
+% tabLocation = strfind(line,sprintf('\t'));
+% profile.zmin = -str2double(line(tabLocation(1):end));
+% line = fgetl(fidLog);
+% tabLocation = strfind(line,sprintf('\t'));
+% profile.zmax = -str2double(line(tabLocation(1):end));
+% line = fgetl(fidLog);
+% tabLocation = strfind(line,sprintf('\t'));
+% profile.res = str2double(line(tabLocation(1):end));
+% 
+% fclose(fidLog);
+% 
+% % open surface depths file
+% fidSurfDep = fopen(strcat('/',directoryName,'/','BasinSurfaceDepths.txt'));
+% 
+% temp = fgetl(fidSurfDep);
+% 
+% count = 1;
+% while ~feof(fidSurfDep)
+%     line = fgetl(fidLog);
+%     tabLocation = strfind(line,sprintf('\t'));
+%     profile.surfName{count} = line(1:tabLocation(1)-2);
+%     profile.surfDep(count) = str2double(line(tabLocation(1):end));
+%     count = count + 1;
+%     
+% end
+% fclose(fidSurfDep);
 
 
 
 % open primary data file
-fidProfile = fopen(strcat('/',directoryName,'/','ProfileAtIndividualLocation.txt'));
+strcat('/',directoryName,'/Profile/','Profile.txt')
+fidProfile = fopen(strcat('/',directoryName,'/Profile/','Profile.txt'))
 
 temp = fgetl(fidProfile);
 temp = fgetl(fidProfile); % discard first two lines

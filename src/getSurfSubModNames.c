974fbbfa3c15dd56eb944f95dc3d5f9b2c77c5e6
Sung Bae <sung.bae@canterbury.ac.nz>
2021-09-30 13:33:12 +1300

    Removed redundant lines

--- src/getSurfSubModNames.c
+++ src/getSurfSubModNames.c
@@ -1356,20 +1356,6 @@ global_model_parameters *getGlobalModelParameters(char *modelVersion, char *TOPO
     else if(strcmp(modelVersion,"1.64") == 0)
     {
         // define the number of surfaces and sub models
-        GLOBAL_MODEL_PARAMETERS->nSurf = 2;
-        GLOBAL_MODEL_PARAMETERS->nVeloSubMod = 1;
-        
-        // insert surface surface keywords and filenames
-        GLOBAL_MODEL_PARAMETERS->surf[0] = "posInfSurf";
-        GLOBAL_MODEL_PARAMETERS->globalSurfFilenames[0] = "Data/Global_Surfaces/posInf.in";
-        GLOBAL_MODEL_PARAMETERS->surf[1] = "negInfSurf";
-        GLOBAL_MODEL_PARAMETERS->globalSurfFilenames[1] = "Data/Global_Surfaces/negInf.in";
-        
-        // insert velocity submodel keywords and filenames (if necessary)
-        GLOBAL_MODEL_PARAMETERS->veloSubMod[0] = "EPtomo2010subMod";
-        GLOBAL_MODEL_PARAMETERS->tomographyName = "2010_Full_South_Island";
-        
-        // define the number of surfaces and sub models
         GLOBAL_MODEL_PARAMETERS->nSurf = 3;
         GLOBAL_MODEL_PARAMETERS->nVeloSubMod = 2;
         

62ee9d7467b57f6db87f6f7c5cca5e8a25ee6953
sungeunbae <sung.bae@canterbury.ac.nz>
2023-07-24 13:34:29 +1200

    Added a new function readParameterNoExit()

--- src/readWriteInputFiles.c
+++ src/readWriteInputFiles.c
@@ -184,45 +184,51 @@ void writeSampleInputTextFiles(void)
 
 char *readParameter(char *fileName, char *quality)
 {
+    returnValue = readParameterNoExit(fileName,quality);
+
+    if (returnValue == NULL)
+    {
+        printf("Quality %s not set in file %s. See readme.\n",quality,fileName);
+        exit(EXIT_FAILURE);
+    }
+    return returnValue;
+}
+
+char *readParameterNoExit(char *fileName, char *quality) {
     char line[MAX_FILENAME_STRING_LEN];
-    char *returnValue=(char*) malloc(MAX_FILENAME_STRING_LEN*sizeof(char));
+    char *returnValue = (char *) malloc(MAX_FILENAME_STRING_LEN * sizeof(char));
     char *type;
     char *val;
     int valAssigned = 0;
 
     FILE *fNameRead = NULL;
-    fNameRead  = fopen(fileName, "r");
-    if (fNameRead == NULL)
-    {
-        printf("File not found. (%s).\n",fileName);
+    fNameRead = fopen(fileName, "r");
+    if (fNameRead == NULL) {
+        printf("File not found. (%s).\n", fileName);
         exit(EXIT_FAILURE);
     }
 
-    while (fgets (line, sizeof(line), fNameRead))
-    {
-        type = strtok (line,"=");
-        val = strtok (NULL,"=");
-        if(strcmp(type, quality) == 0)
-        {
-            val[strlen(val)-1] = 0;
-            strcpy(returnValue,val);
+    while (fgets(line, sizeof(line), fNameRead)) {
+        type = strtok(line, "=");
+        val = strtok(NULL, "=");
+        if (strcmp(type, quality) == 0) {
+            val[strlen(val) - 1] = 0;
+            strcpy(returnValue, val);
 //            printf("%s %s\n",type, val);
             valAssigned = 1;
             break;
-
         }
     }
+    fclose(fNameRead);
+
     if (valAssigned == 0)
     {
-        printf("Quality %s not set in file %s. See readme.\n",quality,fileName);
-        exit(EXIT_FAILURE);
+        returnValue = NULL;
     }
-    fclose(fNameRead);
     return returnValue;
 }
 
-
-gen_extract_velo_mod_call readGenVMInputTextFile(char *fileName, int rank)
+        gen_extract_velo_mod_call readGenVMInputTextFile(char *fileName, int rank)
 {
 
    gen_extract_velo_mod_call GEN_EXTRACT_VELO_MOD_CALL;
@@ -242,8 +248,8 @@ gen_extract_velo_mod_call readGenVMInputTextFile(char *fileName, int rank)
        GEN_EXTRACT_VELO_MOD_CALL.MIN_VS = atof(readParameter(fileName,"MIN_VS"));
        GEN_EXTRACT_VELO_MOD_CALL.TOPO_TYPE = readParameter(fileName,"TOPO_TYPE");
        topo_type_string_length = strnlen(GEN_EXTRACT_VELO_MOD_CALL.TOPO_TYPE, 200);
-       char *model_format=readParameter(fileName,"MODEL_FORMAT");
        GEN_EXTRACT_VELO_MOD_CALL.AWP_OUTPUT = 0;
+       char *model_format=readParameterNoExit(fileName,"MODEL_FORMAT");
        if (strcmp(model_format, "AWP") == 0) GEN_EXTRACT_VELO_MOD_CALL.AWP_OUTPUT = 1;
    }
 
@@ -300,7 +306,7 @@ gen_extract_velo_mod_call readExtractVMInputTextFile(char *fileName)
 gen_velo_slices_call readGenerateSliceInputTextFile(char *fileName)
 {
     gen_velo_slices_call GEN_VELO_SLICES_CALL;
-    GEN_VELO_SLICES_CALL.GENERATED_SLICE_PARAMETERS_TEXTFILE = readParameter(fileName,"GENERATED_SLICE_PARAMETERS_TEXTFILE");
+    GEN_VELO_SLICES_CALL.GENERATED_SLICE_PARAMETERS_TEXTFILE = readParameter(fileName,"GENERATED_SLICE_PARAMETERS_TEXTFILE";
     GEN_VELO_SLICES_CALL.TOPO_TYPE = readParameter(fileName,"TOPO_TYPE");
 
     return GEN_VELO_SLICES_CALL;

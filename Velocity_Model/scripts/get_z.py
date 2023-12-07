736be13c9b03fdf680dd6293156d7ef244c52b58
Sung Bae <sung.bae@nesi.org.nz>
2023-12-07 23:05:08 +0000

    Compute both Z1.0 and Z2.5 by default

--- Velocity_Model/scripts/get_z.py
+++ Velocity_Model/scripts/get_z.py
@@ -2,23 +2,13 @@ import argparse
 from pathlib import Path
 
 from qcore import formats
-from Velocity_Model.z import extract_z, basin_outlines_dict 
+from Velocity_Model.z import extract_z, basin_outlines_dict
+
+z_types = ["Z1.0", "Z2.5"]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-ll", help="Station ll file", required=True, type=Path)
-    parser.add_argument(
-        "-z",
-        "--z-types",
-        choices=[
-            "Z1.0",
-            "Z2.5",
-        ],  # TO DO:["vs30", "vs500"] not working despite documentation saying otherwise
-        help="Selects what Z type to extract: [%(choices)s]",
-        action="extend",
-        nargs="+",
-        required=True,
-    )
     vm_versions = basin_outlines_dict.keys()
     parser.add_argument(
         "-v",
@@ -34,6 +24,8 @@ if __name__ == "__main__":
         default=True,
         action="store_false",
     )
+    # by default, the output file will be current_dir / {ll_file}.z
+    # this is to avoid overwriting existing z-file in StationInfo directory by mistake
     parser.add_argument("-o", "--outdir", help="output directory", default=Path.cwd(), type=Path)
     parser.add_argument("--nzvm-path", default="NZVM", type=Path)
     args = parser.parse_args()
@@ -43,9 +35,8 @@ if __name__ == "__main__":
     print(args.outdir)
     output_file = args.outdir / args.ll.with_suffix(".z").name
 
-    print(f"Specified z-types : {args.z_types}")
     print(f"Output to be saved as {output_file}")
 
-    z_df = extract_z(args.z_types, stat_df, args.nzvm_path, args.version)
+    z_df = extract_z(z_types, stat_df, args.nzvm_path, args.version)
     print(z_df)
     z_df.to_csv(output_file,header=args.keep_header)
